5cb4ee4e30fc47821c2d0e06857812fcb3217f59
alebzak <ceigh@pm.me>
2019-10-25 15:12:33 +0500

    (feat): Make command decrease points on StreamElements

--- src/app.js
+++ src/app.js
@@ -1,18 +1,32 @@
 #!/usr/bin/env node
 
-
+// Requires
 require('dotenv').config();
-
 const tmi = require('tmi.js');
 const phrases = require('./phrases');
 
 
+// Variables
 const bot = process.env.BOT;
 if (!bot) throw Error('Define BOT env variable');
 const channel = process.env.CHANNEL;
 if (!channel) throw new Error('Define CHANNEL env variable');
-const token = process.env.TOKEN;
-if (!token) throw new Error('Define TOKEN env variable');
+const twitchToken = process.env.TWITCH_TOKEN;
+if (!twitchToken) throw new Error('Define TWITCH_TOKEN env variable');
+const twitchClientId = process.env.TWITCH_CLIENT_ID;
+if (!twitchClientId) console.warn('If you want to use auto skip value '
+  + 'define TWITCH_CLIENT_ID env variable');
+const streamElementsJwt = process.env.STREAMELEMENTS_JWT_TOKEN;
+const streamElementsAid = process.env.STREAMELEMENTS_ACCOUNT_ID;
+if (!streamElementsAid || !streamElementsJwt) console.warn('If you want '
+  + 'to spend points by command define STREAMELEMENTS_JWT_TOKEN env variable');
+
+let total = 0;
+const data = {
+  skip: 4,
+  curr: 0,
+  usrs: []
+};
 
 const options = {
   options: {
@@ -24,31 +38,51 @@ const options = {
   },
   identity: {
     username: bot,
-    password: token
+    password: twitchToken
   },
   channels: [channel]
 };
 const client = new tmi.client(options);
 
-const data = {
-  skip: 4,
-  curr: 0,
-  usrs: []
-};
 
-const clientID = process.env.CLIENT_ID;
+// Executions
+if (require.main === module) {
+  client.on('chat', (chl, usr, msg) => {
+    checkSetSkip(chl, usr, msg);
+    checkSkipMsg(chl, usr, msg);
+    updateTotal();
+    console.info(data);
+  });
 
-
-if (clientID) {
-  client.api({
-    url: `https://api.twitch.tv/kraken/streams/${channel}?client_id=${clientID}`
-  }, (err, res, body) => {
-    if (body.stream) data.skip = Math.round(body.stream.viewers / 20);
- });
+  setSkipByViewers();
+  client.connect();
 }
 
-client.on('chat', (chl, usr, msg) => {
-  // Set toSkip
+
+// Functions
+function updateTotal() {
+  total++;
+  if (total === 100) {
+    setSkipByViewers();
+    total = 0;
+  }
+}
+
+function setSkipByViewers() {
+  if (twitchClientId) {
+    client.api({
+      url: `https://api.twitch.tv/kraken/streams/${channel}?client_id=${twitchClientId}`
+    }, (e, r, b) => {
+      if (b.stream) {
+        const newVal = Math.round(b.stream.viewers / 25);
+        data.skip = newVal;
+        console.log(`Set new skip amount to: ${newVal}`);
+      } else if (e) console.warn(e);
+    });
+  }
+}
+
+function checkSetSkip(chl, usr, msg) {
   if (usr.mod || (usr.badges && usr.badges.broadcaster)) {
     const match = msg.match(/^!говно (\d+)$/);
     if (match) {
@@ -57,19 +91,20 @@ client.on('chat', (chl, usr, msg) => {
       client.action(chl, `Для скипа теперь нужно ${val} нуиговен :O`);
     }
   }
+}
 
-  // Handle skip msgs
+function checkSkipMsg(chl, usr, msg) {
   const low = msg.toLowerCase();
   const rxp = /^(ну ?и ?г[aаo0о]вн[o0о]|n[uy] ?i ?g[aаo0о]vn[o0о]).*/;
 
-  if (rxp.test(low) && !low.includes('не')) {
+  if (rxp.test(low)) {
     if (!data.usrs.includes(usr.username)) {
       data.curr++;
       data.usrs.push(usr.username);
-
       if (data.curr === data.skip) {
         client.action(chl, `${phrases.getPhrase()} @${channel}`);
         data.curr = 0;
+        addPoints(data.usrs, -100);
         data.usrs = [];
       }
     }
@@ -77,10 +112,19 @@ client.on('chat', (chl, usr, msg) => {
     data.curr = 0;
     data.usrs = [];
   }
+}
 
-  console.info(data);
-});
-
-client.connect();
-console.info(data);
+function addPoints(usrs, points) {
+  if (streamElementsJwt && streamElementsAid) {
+    usrs.forEach(u => {
+      client.api({
+        url: `https://api.streamelements.com/kappa/v2/points/${streamElementsAid}/${u}/${points}`,
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${streamElementsJwt}`
+        },
+      }, (e, r, b) => e ? console.warn(e) : console.log(b.message))
+    });
+  }
+}
 
